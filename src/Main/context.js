1ceab487a1c065bb088dc96039879bdfd9b9d3a3
Jordi Gómez <jordi.gomez@ext.aqualogybs.net>
2019-06-18 10:07:12 +0200

    add local storage

--- src/Main/context.js
+++ src/Main/context.js
@@ -1,18 +1,31 @@
 import React, { createContext, useState, useEffect } from "react";
+import { isArray } from "util";
+
+function MyError(message) {
+  this.name = "error";
+}
 
 const APIContext = createContext([]);
 
 const APIProvider = ({ children }) => {
   const [habitants, setHabitants] = useState([]);
 
-  useEffect(() => {
-    async function fetchData() {
-      const response = await fetch(process.env.REACT_APP_API_URL)
-        .then(response => response.json())
-        .then(({ Brastlewark }) => Brastlewark);
-      console.log(response);
+  async function fetchData() {
+    const localData = localStorage.getItem("habitantsLocal");
+    localData && setHabitants(JSON.parse(localData));
+
+    const response = await fetch(process.env.REACT_APP_API_URL)
+      .then(response => response.json())
+      .then(({ Brastlewark }) => Brastlewark)
+      .catch(error => console.error(error.message));
+
+    if (response && isArray(response)) {
       setHabitants(response);
+      localStorage.setItem("habitantsLocal", JSON.stringify(response));
     }
+  }
+
+  useEffect(() => {
     fetchData();
   }, []);
 
