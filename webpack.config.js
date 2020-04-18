e71bb49c7ea5e6dc5fc47824085c347542f8fcc9
Jordi Gomper <jordigomper@gmail.com>
2020-04-18 23:01:31 +0200

    :truck: moved icon and manifest to webpack confing

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const InterpolateHtmlPlugin = require("interpolate-html-plugin");
+const ManifestPlugin = require("webpack-manifest-plugin");
 
 const javascriptRules = {
   test: /\.js$/,
@@ -34,6 +34,24 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "./public/index.html",
+      favicon: "./public/favicon.ico",
+    }),
+    new ManifestPlugin({
+      seed: {
+        short_name: "GnomeBook",
+        name: "Social gnome application",
+        icons: [
+          {
+            src: "favicon.ico",
+            sizes: "64x64 32x32 24x24 16x16",
+            type: "image/x-icon",
+          },
+        ],
+        start_url: ".",
+        display: "standalone",
+        theme_color: "#4267b2",
+        background_color: "#ffffff",
+      },
     }),
   ],
 };
