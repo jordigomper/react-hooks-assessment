a7c59a1f4d8b0df658b95ccd6d656eab264cc35b
Jordi Gomper <jordigomper@gmail.com>
2020-04-22 00:52:33 +0200

    :fire: deleted unused code

--- src/pages/layouts/Header/index.js
+++ src/pages/layouts/Header/index.js
@@ -1,6 +1,4 @@
-import React, { useEffect, useState } from "react";
-import PropTypes from "prop-types";
-
+import React from "react";
 import { Container } from "./elements";
 
 export const Header = ({ children }) => {
